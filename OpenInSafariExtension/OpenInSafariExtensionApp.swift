21551e01a03f8814f2bff8ded8085ee68f1ff17f
Alexandre Fenyo <github@fenyo.net>
2025-10-19 18:48:42 +0200

    Initial Commit

---
+++ OpenInSafariExtension/OpenInSafariExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenInSafariExtensionApp.swift
+//  OpenInSafariExtension
+//
+//  Created by Alexandre Fenyo on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct OpenInSafariExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
